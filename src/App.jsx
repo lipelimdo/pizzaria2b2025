74943b179654da9e83a7b31bc635485b17a07aca
lipelimdo <rm94298@estudante.fieb.edu.br>
2025-03-31 08:29:46 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,43 @@ import {useState} from "react"
 
 function App (){
 
-  const [nome, setNome] = useState ('Felipe')
+  
 
-  return (
-    <div>
-    <h3> Pizzaria 2B</h3>
-    <input className="nome"
+  const Formulario = (props) => {
+
+    const [nome, setNome] = useState ('Felipe')
+    return (
+      <div>
+        <h3>{props.titulo}</h3>
+        <input className="nome"
     placeholder="Digite seu nome..."
     onChange={(e)=>{setNome(e.target.value)}}
     name="nome"
     type="text" />
-    <button className="botao" onClick={()=>alert(nome)}>
+    <button className="botao" onClick={
+      () => { 
+         if(nome.length > 0)
+           alert(nome) 
+         else
+           alert(props.mensagem)
+       
+         }  
+       }>
+     
       CLIQUE AQUI
     </button>
+      
+    </div>
+    )
+  }
+
+  return (
+    <div>
+    <h3> Pizzaria 2B</h3>
+   <Formulario titulo = "Nome" mensagem = "Baleia" />
+   <Formulario titulo = "E-mail" mensagem = "Sem Mundial" />
+   <Formulario titulo = "CPF" mensagem = "São Paulo Futebol Clube" />
+   <Formulario />
     </div>
 
   )
